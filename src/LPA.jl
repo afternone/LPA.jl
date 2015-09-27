fd3c021b72ac296c65070340340ba65fc98e21cc
afternone <afternone@163.com>
2015-09-27 14:46:35 +0800

    fix voi.jl

--- src/LPA.jl
+++ src/LPA.jl
@@ -8,7 +8,7 @@ export label_propagation, modularity, nmi, voi
 include("labelpropagation.jl")
 include("modularity.jl")
 include("nmi.jl")
-include("voi")
+include("voi.jl")
 include("utils.jl")
 
 end # module
